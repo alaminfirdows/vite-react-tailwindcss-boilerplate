2714bae30f7df77c965507ac1b063cd3e51f67bc
Al-Amin Firdows <alaminfirdows@gmail.com>
2022-08-08 09:50:22 +0600

    init boilerplate project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+	return (
+		<div className='bg-gray-50 grid w-screen h-screen place-content-center text-center'>
+			<h2 className='text-2xl font-semibold mb-4'>Vite-React-Tailwind</h2>
+			<p>
+				Just another boilerplate for
+				<a href='https://vitejs.dev/'>Vite.js</a>,
+				<a href='https://reactjs.org/'>React.js</a> and
+				<a href='https://tailwindcss.com/'>Tailwindcss</a>
+			</p>
+
+			<div className='bg-zinc-200 rounded px-4 py-3 text-sm font-mono mt-10'>
+				git@github.com:com:alaminfirdows/vite-react-tailwindcss-boilerplate.git
+			</div>
+		</div>
+	);
+}
+
+export default App;
