7f502018ce24ff25e7e630734dc52e238fcf6460
Al-Amin Firdows <alaminfirdows@gmail.com>
2022-08-08 10:41:25 +0600

    feat: update github url

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,21 @@ function App() {
 			</p>
 
 			<div className='bg-zinc-100 border-zinc-200 border rounded px-6 py-4 text-sm font-mono mt-14'>
-				git@github.com:com:alaminfirdows/vite-react-tailwindcss-boilerplate.git
+				npx degit alaminfirdows/vite-react-tailwindcss-boilerplate.git
+				my-app
 			</div>
 
 			<div className='mt-6'>
-				<button className='github'>
+				<a
+					href='https://github.com/alaminfirdows/vite-react-tailwindcss-boilerplate'
+					target={'_blank'}
+					className='github'>
 					<span className='font-semibold'>View on</span>
 					<img
 						src='https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png'
 						className='h-4'
 					/>
-				</button>
+				</a>
 			</div>
 		</div>
 	);
