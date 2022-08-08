2fe3a7f899fdebcb6d62b6f21ca0257f353aad98
Al-Amin Firdows <alaminfirdows@gmail.com>
2022-08-08 10:09:34 +0600

    feat: add github button

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,18 @@ function App() {
 				<a href='https://tailwindcss.com/'>Tailwindcss</a>
 			</p>
 
-			<div className='bg-zinc-200 rounded px-4 py-3 text-sm font-mono mt-10'>
+			<div className='bg-zinc-100 border-zinc-200 border rounded px-6 py-4 text-sm font-mono mt-14'>
 				git@github.com:com:alaminfirdows/vite-react-tailwindcss-boilerplate.git
+			</div>
+
+			<div className='mt-6'>
+				<button className='github'>
+					<span className='font-semibold'>View on</span>
+					<img
+						src='https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png'
+						className='h-4'
+					/>
+				</button>
 			</div>
 		</div>
 	);
